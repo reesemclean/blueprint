e7e83747da9ae4f15e5a434eefbca87978e69620
Reese McLean <reese.mclean@me.com>
2018-05-06 23:21:30 -0700

    Finish rework of file creator.

--- src/fileCreator/createFiles.ts
+++ src/fileCreator/createFiles.ts
@@ -1,9 +1,9 @@
 "use strict";
 
-import * as fs from "fs";
+import * as fs from "fs-extra";
 import * as handlebars from "handlebars";
 import * as _ from "lodash";
-import * as mkdirp from "mkdirp";
+import * as os from "os";
 import * as path from "path";
 
 import * as constants from "../constants";
@@ -41,36 +41,65 @@ export async function createFiles(userInput: IUserInput, inDirectory: string) {
 
   const name = sanitizedName(userInput.inputName, options.suffixesToIgnoreInInput);
 
-  let directoryToCreateIn = inDirectory;
+  const temporaryDirectory = fs.mkdtempSync(path.join(os.tmpdir(), 'blueprint-'));
+
+  createFilesFromTemplateInDirectory(
+    userInput.selectedTemplatePath,
+    temporaryDirectory,
+    userInput.inputName
+  );
+
+  let containerFolderName = "";
   if (options.createFilesInFolderWithPattern) {
-    directoryToCreateIn = await createContainerFolder(options.createFilesInFolderWithPattern, name, inDirectory);
+    containerFolderName = replaceName(options.createFilesInFolderWithPattern, name);
   }
 
-  const folderPaths = await folderPathsToCreate(userInput.selectedTemplatePath, directoryToCreateIn, name);
-  const filePaths = await filePathsToCreate(folderPaths, userInput.selectedTemplatePath, directoryToCreateIn, name);
+  const pathToMoveTo = path.join(inDirectory, containerFolderName);
+  const foldersToMove = getFolderNamesAtDirectory(temporaryDirectory);
 
-  await createFolders(folderPaths, directoryToCreateIn);
-  await createFilesFromTemplateFiles(
-    templateFilePaths(folderPaths, userInput.selectedTemplatePath),
-    userInput.selectedTemplatePath,
-    filePaths,
-    directoryToCreateIn,
-    name
-  );
+  const folderNames = fs.readdirSync(temporaryDirectory).forEach(fileOrFolderName => {
+    const pathToContents = path.join(temporaryDirectory, fileOrFolderName);
+    const finalPath = path.join(pathToMoveTo, fileOrFolderName);
+
+    try {
+      fs.copySync(pathToContents, finalPath, { overwrite: false, errorOnExist: true, recursive: true });
+    } catch (e) {
+      if (e.code === 'EEXIST') {
+        throw new Error(`File already exists at path: ${finalPath}`);
+      }
+
+      throw e;
+    }
+  });
+
 }
 
-async function createFolders(folderPaths: string[], inDirectory: string) {
-  mkdirp.sync(inDirectory);
-  folderPaths.forEach((folderPath) => mkdirp.sync(folderPath));
+function createFilesFromTemplateInDirectory(templatePath: string, inDirectory: string, name: string) {
+
+  createFilesForTemplateFolder(templatePath, inDirectory, name);
+
+  const templateFoldersInFolder = getFolderNamesAtDirectory(templatePath);
+
+  templateFoldersInFolder.forEach((templateFolderName) => {
+
+    const folderName = replaceName(templateFolderName, name);
+    const fullFolderPath = path.join(templatePath, templateFolderName);
+    const directoryToCreateIn = path.join(inDirectory, folderName);
+    fs.mkdirSync(directoryToCreateIn);
+    createFilesFromTemplateInDirectory(fullFolderPath, directoryToCreateIn, name);
+
+  });
+
 }
 
-async function createFilesFromTemplateFiles(templateFilePaths: string[], templateDirectory: string, filePaths: string[], inDirectory: string, name: string) {
-  const templateFileNameToFilePathToCreateMapping = _.zipObject(templateFilePaths, filePaths);
+async function createFilesForTemplateFolder(templateFolderPath: string, inDirectory: string, name: string) {
 
-  Object.keys(templateFileNameToFilePathToCreateMapping).forEach((templateFilePath) => {
+  const templateFilesInFolder = getTemplateFileNamesAtTemplateDirectory(templateFolderPath);
+
+  templateFilesInFolder.forEach(templateFilePath => {
 
     const rawTemplateContent = fs.readFileSync(
-      path.join(templateDirectory, templateFilePath),
+      path.join(templateFolderPath, templateFilePath),
       "utf8",
     );
     const template = handlebars.compile(rawTemplateContent);
@@ -81,62 +110,11 @@ async function createFilesFromTemplateFiles(templateFilePaths: string[], templat
 
     const content = template(context);
 
-    fs.writeFileSync(templateFileNameToFilePathToCreateMapping[templateFilePath], content);
+    const fileName = replaceName(templateFilePath, name);
+    fs.writeFileSync(path.join(inDirectory, fileName), content);
 
   });
-}
 
-async function folderPathsToCreate(fromDirectory: string, intoDirectory: string, name: string): Promise<string[]> {
-  const templateFolderPaths = getFolderPathsRecursively(fromDirectory);
-  const folderPathsToCreate = templateFolderPaths.map((templateFolderPath) => {
-    return path.join(intoDirectory, replaceName(templateFolderPath, name));
-  });
-
-  let conflictingFolderPath: string;
-  for (const folderPath of folderPathsToCreate) {
-    if (fs.existsSync(folderPath)) {
-      conflictingFolderPath = folderPath;
-      break;
-    }
-  }
-
-  if (conflictingFolderPath) {
-    throw new Error(`Folder already exists at path: ${conflictingFolderPath}`);
-  }
-
-  return folderPathsToCreate;
-}
-
-async function filePathsToCreate(folderPaths: string[], fromDirectory: string, intoDirectory: string, name: string): Promise<string[]> {
-
-  const filePathsToCreate: string[] = templateFilePaths(folderPaths, fromDirectory)
-    .map((templateFilePath) => {
-      return path.join(intoDirectory, replaceName(templateFilePath, name));
-    });
-
-  let conflictingFilePath: string;
-  for (const filePath of filePathsToCreate) {
-    if (fs.existsSync(filePath)) {
-      conflictingFilePath = filePath;
-      break;
-    }
-  }
-
-  if (conflictingFilePath) {
-    throw new Error(`File already exists at path: ${conflictingFilePath}`);
-  }
-
-  return filePathsToCreate;
-}
-
-function templateFilePaths(folderPaths: string[], fromDirectory: string): string[] {
-  return _.flatMap(folderPaths.concat([""]), (folderPath) => {
-    const fullTemplateFolderPath = path.join(fromDirectory, folderPath);
-    const templateFileNames = getTemplateFileNamesAtTemplateDirectory(fullTemplateFolderPath);
-    return templateFileNames.map((templateFileName) => {
-      return path.join(folderPath, templateFileName);
-    });
-  });
 }
 
 function sanitizedName(name: string, ignoreSuffixes: string[]): string {
@@ -150,20 +128,6 @@ function sanitizedName(name: string, ignoreSuffixes: string[]): string {
   }
 
   return nameToUse;
-}
-
-async function createContainerFolder(folderPattern: string, templateName: string, directoryToCreateIn: string): Promise<string> {
-
-  const folderName = replaceName(folderPattern, templateName);
-  const directory = path.join(directoryToCreateIn, folderName);
-  const pathExists = fs.existsSync(directory);
-
-  if (pathExists) {
-    throw new Error(`Folder already exists at path: ${directory}`);
-  }
-
-  return directory;
-
 }
 
 function escapeRegExp(str): string {
@@ -183,21 +147,6 @@ function replaceName(stringToReplace: string, name: string): string {
   result = replaceAll(result, "__upperCase_name__", _.upperCase(name));
   result = replaceAll(result, "__lowerCase_name__", _.lowerCase(name));
   return result;
-}
-
-function getFolderPathsRecursively(directoryPath: string, existingPath: string = ""): string[] {
-  const templatePath = path.join(directoryPath, existingPath);
-  const templateFolderNames = getFolderNamesAtDirectory(templatePath);
-  const folderPathsAtThisLevel = templateFolderNames.map((templateFolderName) => {
-    if (existingPath) {
-      return path.join(existingPath, templateFolderName);
-    }
-    return templateFolderName;
-  });
-
-  return folderPathsAtThisLevel.concat(_.flatMap(folderPathsAtThisLevel, (folderPath) => {
-    return getFolderPathsRecursively(directoryPath, folderPath);
-  }));
 }
 
 function getFolderNamesAtDirectory(directoryPath: string): string[] {
